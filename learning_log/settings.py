fb934f850bd710bfdeb76da7ca57b3d6f999361d
waflia <waflias@yandex.ru>
2020-02-09 14:13:56 +0300

    Third try

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -141,7 +141,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
